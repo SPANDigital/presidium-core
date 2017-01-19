82da972a6b9d63990704e53dc5fb39785a14724b
dominicfollett <dominic.follett.smith@gmail.com>
2017-01-19 10:08:05 +0200

    Presidium core code does not require any compiled version in its repo.

--- versions/latest/webpack/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import ListReducer from './reducer_list';
-
-const rootReducer = combineReducers({
-    app: ListReducer
-});
-
-export default rootReducer;
-
