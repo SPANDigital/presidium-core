5865cbc8591187a82e4b136488c8182cdc87234f
Paco Mendes <paco.mendes@spandigital.com>
2017-02-13 19:19:28 +0200

    Requirements check script

--- bin/requirements.sh
+++ bin/requirements.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# CHECKS PREREQUISITES TO RUN JEKYLL LOCALLY
+# CHECK PREREQUISITES TO RUN JEKYLL
 
 echo
-echo Checking requirements to run Presidium locally...
+echo Checking requirements to run Jekyll for Presidium...
 echo
 
 if  ! type "ruby" > /dev/null;
@@ -18,9 +18,11 @@ then
 elif ! type "bundle" > /dev/null;
 then
     echo
+    echo '####################################'
     echo 'BUNDLER NOT FOUND'
     echo 'Please install bundler to continue...'
     echo '`gem install bundler`'
+    echo '####################################'
     echo
     exit 1
 elif [[ $(ruby -v) < "ruby 2.1" ]]
@@ -35,7 +37,7 @@ then
     echo
     exit 1
 else
-  echo 'Setting up Presidium with:'
+  echo 'Using:'
   echo $(ruby -v)
   echo $(bundle -v)
   echo
