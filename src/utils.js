44b04a8a5fc2bc725ba5d1c5d4d12feeb6439d28
Ruaan van der Spuy <rrvds87@gmail.com>
2018-05-07 07:33:46 -0700

    Added utils.js

---
+++ src/utils.js
@@ -0,0 +1,11 @@
+var utils = module.exports;
+
+utils.contains = function (a, obj) {
+    var i = a.length;
+    while (i--) {
+        if (a[i] === obj) {
+            return true;
+        }
+    }
+    return false;
+}
