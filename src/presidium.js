0aeac01cfedbe045d27367a7b98d888fe548c086
David Berliner <dsberliner@gmail.com>
2019-02-28 19:26:37 +1300

    Allow img tags in XSS check.

--- src/presidium.js
+++ src/presidium.js
@@ -80,10 +80,11 @@ presidium.sanitize = function(conf) {
         return sanitizeHtml(dirty, {
             allowedTags: [ 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
                 'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div',
-                'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre'],
+                'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre', 'img'],
             allowedAttributes: {
                 '*': ['class', 'data-*', 'href', 'align', 'alt', 'center', 'bgcolor', 
                     'id', 'style', 'name', 'target' ],
+                'img': ['src']
             }
         });
     }
